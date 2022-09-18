61d0f20bca1046d75b261ec84623671b99bf67a7
Stanislav Sandler <stanislavsandler@Stanislavs-MacBook-Air.local>
2022-09-18 12:32:09 +0200

    Implementirao kretanje igraca

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 const platno = document.getElementById("platno");
 const kontekst = platno.getContext("2d");
+kontekst.translate(0.5, 0.5);
+const fps = 60;
+
+const bojaPokreta = "#DDB892";
+const bojaZida = "#CCD5AE";
+const bojaPozadine = "#E9EDC9";
+const bojaIgraca = "#D4A373";
+//const bojaOkviraIgraca = "#FFCFAD";
 /* STA TREBA URADITI:
-	- kretanje igraca
+	- kretanje igraca GOTOVO
 	- Vestacka Inteligencija (igrac 2)
 	- drag&drop?
 	- NIM-slicne igre
 */
 
+
+function nadjiNajdesnijuNulu(broj){
+	broj = ~broj;
+	let stepen = (broj&~(broj-1));
+	let odgovor = 0;
+	while(stepen){
+		stepen/=2;
+		odgovor++;
+	}
+	return odgovor;
+}
 
 class Tabla{
 	constructor(dimenzija, velicina){
@@ -22,15 +41,15 @@ class Tabla{
 		}
 	}
 	crtaj(){
-		kontekst.strokeStye = "black";
-		kontekst.lineWidth = 2.5;
+		kontekst.strokeStyle = bojaZida;
+		kontekst.fillStyle = bojaZida;
+		//kontekst.lineWidth = 2.5;
 		for(let i=0;i<this.dimenzija;i++){
 			for(let j = 0;j<this.dimenzija;j++){
 				if(!this.matrica[i][j]){
-					kontekst.fillStyle = "black";
-					kontekst.fillRect(i*this.strana, j*this.strana, this.strana, this.strana);
+					kontekst.fillRect(j*this.strana, i*this.strana, this.strana, this.strana);
 				}
-				kontekst.strokeRect(i*this.strana, j*this.strana, this.strana, this.strana);
+				kontekst.strokeRect(j*this.strana, i*this.strana, this.strana, this.strana);
 				
 			}
 		}
@@ -81,6 +100,11 @@ class Tabla{
 			});
 		}
 	}
+	izracunajGrandi(){
+		let red = [];
+		red.push([0,0,0]);
+
+	}
 }
 
 class Igrac{
@@ -88,34 +112,82 @@ class Igrac{
 		this.tbl = tbl;
 		this.x = tbl.dimenzija-1;
 		this.y = tbl.dimenzija-1;
+		this.najdaljiX = this.x;
+		for(let i=this.x;i>=0;i--){
+			if(this.tbl.matrica[this.y][i]==0)break;
+			this.najdaljiX=i;
+		}
+		this.najdaljiY = this.y;
+		for(let i=this.y;i>=0;i--){
+			if(this.tbl.matrica[i][this.x]==0)break;
+			this.najdaljiY=i;
+		}
 	}
 	crtaj(){
-		kontekst.fillStyle = "grey";
-		kontekst.strokeStyle = "black";
+		
+		kontekst.fillStyle = bojaIgraca;
+		//kontekst.strokeStyle = bojaOkviraIgraca;
 		let centarX = this.tbl.strana * this.x + this.tbl.strana/2;
 		let centarY = this.tbl.strana * this.y + this.tbl.strana/2;
-		kontekst.arc(centarX, centarY, this.tbl.strana/2, 0, Math.PI * 2);
-		kontekst.stroke();
+		kontekst.beginPath();
+		kontekst.arc(centarX, centarY, this.tbl.strana/2 * 0.8, 0, Math.PI * 2);
+		//kontekst.stroke();
+		kontekst.fill();
+		kontekst.beginPath();
+		kontekst.fillStyle = bojaPokreta;
+		for(let i = this.najdaljiX; i<this.x; i++){
+			let cntr = this.tbl.strana * i + this.tbl.strana/2;
+			kontekst.arc(cntr, centarY, this.tbl.strana/4, 0, Math.PI * 2);
+		}
+		kontekst.fill();
+		kontekst.beginPath();
+		for(let i = this.najdaljiY; i<this.y; i++){
+			let cntr = this.tbl.strana * i + this.tbl.strana/2;
+			kontekst.arc(centarX, cntr, this.tbl.strana/4, 0, Math.PI * 2);
+		}
+		//kontekst.stroke();
 		kontekst.fill();
 	}
-	move(noviX, noviY){
+	
+	pomeri(klikX, klikY){
+		let noviX = Math.floor(klikX/this.tbl.strana);
+		let noviY = Math.floor(klikY/this.tbl.strana);
+		if(noviX==this.x && noviY==this.y)return;
+		if(noviX!=this.x && noviY!=this.y)return;
+		if(noviX<this.najdaljiX || noviY<this.najdaljiY)return;
+		if(noviX>this.x || noviY > this.y)return;
 		this.x = noviX;
 		this.y = noviY;
+		this.najdaljiX=this.x;
+		this.najdaljiY=this.y;
+		for(let i = this.x; i>=0; i--){
+			if(this.tbl.matrica[this.y][i]==0)break;
+			this.najdaljiX = i;
+		}
+		for(let i = this.y; i>=0; i--){
+			if(this.tbl.matrica[i][this.x]==0)break;
+			this.najdaljiY = i;
+		}
 	}
 }
-let tbl;
+
+let tabl;
 let igrc;
+platno.addEventListener('click', function(event){
+	igrc.pomeri(event.offsetX, event.offsetY);
+});
 function podesi(){
-	tbl = new Tabla(6, 500);
-	igrc = new Igrac(tbl);
-	tbl.generisiPut();
+	tabl = new Tabla(6, 500);
+	tabl.generisiPut();
+	igrc = new Igrac(tabl);
 }
 podesi();
 function petlja(){
-	tbl.crtaj();
+	kontekst.fillStyle = bojaPozadine;
+	kontekst.fillRect(0,0,500,500);
+	tabl.crtaj();
 	igrc.crtaj();
+	window.requestAnimationFrame(petlja);
 }
-
-
-setInterval(petlja, 1000/60);
+window.requestAnimationFrame(petlja);
 
